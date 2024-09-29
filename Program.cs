f883162a23f42417e8973ef1fb8993673a25ac26
Eetu <99178022+E3TU@users.noreply.github.com>
2024-09-29 14:41:35 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res;
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter symbol: ");
+            string symbol = Console.ReadLine() ?? "default value";
+
+            switch (symbol)
+            {
+                case "+":
+                    res = num1 + num2;
+                    Console.WriteLine("Result: " + res);
+                    break;
+                case "-" :
+                    res = num1 - num2;
+                    Console.WriteLine("Result: " + res);
+                    break;
+                case "*":
+                    res = num1 * num2;
+                    Console.WriteLine("Result: " + res);
+                    break;
+                case "/":
+                    res = num1 / num2;
+                    Console.WriteLine("Result: " + res);
+                    break; 
+                default:
+                    Console.WriteLine("Invalid symbol");
+                    break;
+            }
+        }
+    }
+}
