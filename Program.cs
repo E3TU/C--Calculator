8d8edb3b1a83d03df6c206bdeb9cfcfab9a53fff
Eetu <99178022+E3TU@users.noreply.github.com>
2024-09-29 15:04:36 +0300

    Added do while loop

--- Program.cs
+++ Program.cs
@@ -6,36 +6,46 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            int res;
-            Console.Write("Enter first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter symbol: ");
-            string symbol = Console.ReadLine() ?? "default value";
+            int num1, num2, res;
+            string symbol;
+            char continueCalculating;
 
-            switch (symbol)
+            do
             {
-                case "+":
-                    res = num1 + num2;
-                    Console.WriteLine("Result: " + res);
-                    break;
-                case "-" :
-                    res = num1 - num2;
-                    Console.WriteLine("Result: " + res);
-                    break;
-                case "*":
-                    res = num1 * num2;
-                    Console.WriteLine("Result: " + res);
-                    break;
-                case "/":
-                    res = num1 / num2;
-                    Console.WriteLine("Result: " + res);
-                    break; 
-                default:
-                    Console.WriteLine("Invalid symbol");
-                    break;
-            }
+                Console.Write("Enter first number: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter symbol: ");
+                symbol = Console.ReadLine() ?? "default value";
+
+                switch (symbol)
+                {
+                    case "+":
+                        res = num1 + num2;
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    case "-":
+                        res = num1 - num2;
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    case "*":
+                        res = num1 * num2;
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    case "/":
+                        res = num1 / num2;
+                        Console.WriteLine("Result: " + res);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid symbol");
+                        break;
+                }
+                Console.Write("Do you want to continue using calculator? (y/n): ");
+                continueCalculating = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            } while (char.ToLower(continueCalculating) == 'y');
+            Console.WriteLine("Thank you for using the calculator!");
         }
     }
 }
